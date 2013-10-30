1b9b141f14896114134070bf3fe68a52ab41e0eb
df007df <duanfeisj@gmail.com>
2013-10-30 23:41:52 +0800

    columu 封装完毕

--- typemodule/base.php
+++ typemodule/base.php
@@ -3,8 +3,28 @@
 abstract class baseChart
 {
     
+    public $chartAttr = array();
     
     protected $_typeId = '';
+    
+    protected $_is3D = true;
+    
+    protected $_pendingData = array();//待处理数据
+    /**
+     * 每个图形 唯一的配置数据回调方法
+     * @var type
+     */
+    public $extendsDataAction = array(); 
+    
+    /**
+     * 前端初始化基本参数
+     * @var type 
+     */
+    public $insertChartsAttr = array(
+        'dataFormat' => 'json', 
+        'width' => '60%',
+        'height' => '60%'
+    );
 
 
     private static $_typeobj = array();
@@ -12,7 +32,7 @@ abstract class baseChart
     function __construct() {
         ;
     }
-    
+   
     
     
     public static function factory($type) 
@@ -25,10 +45,174 @@ abstract class baseChart
     }
     
     
+    /**
+     * 获取前端渲染唯一id
+     * @return type
+     */
+    protected function getChartId()
+    {
+        return 'chart_' . substr(md5(time()), 3, 8 );
+    }
+
+
+    public function setInsertChartsAttr($key, $value) 
+    {
+        $this->insertChartsAttr[$key] = $value;
+    }
+
+
+    public function setData($data = array()) 
+    {
+        $this->_pendingData = $data;
+    }   
     
+    /**
+     * 返回 不同类型chart前端渲染需要的数据格式
+     * @param type $data
+     */
+    public function getData($data = array()) 
+    {
+        return $this->_pendingData;
+    } 
+
+
+    public function set3D($bool = true)
+    {
+        $this->_is3D = $bool;
+    }       
+    
+    
+    public function getType() 
+    { 
+        
+        if ($this->_is3D) {
+            return $this->_typeId . '3D';
+        } else {
+            return $this->_typeId . '2D';
+        }
+        
+    }
+
+
+    public function getChartAttr()
+    {
+        return $this->chartAttr;
+    }  
+    
+    
+    
+    public function parseChartAttr($attrs = array())
+    {
+        return $attrs;
+    }
+    
+    
+    public function getExtendsDataAction()
+    {
+        $exts = array();
+        foreach ($this->extendsDataAction as $extaction) {
+            if (method_exists($this, $extaction)) {
+                $exts = array_merge($exts, $this->$extaction());
+            }
+        }
+        return $exts;
+    }        
+
+
+    
+
+    /**
+     * 解析，组合图形配置参数
+     */
+    public function getAttr()
+    {
+        $attr = array();
+        $attr['chart'] = $this->getChartAttr();
+        return array_merge($attr, $this->getExtendsDataAction()); 
+    }  
+    
+    
+    /**
+     * 解析，组合图形配置参数
+     */
+    public function getDataSource()
+    {
+        $attr = $this->getAttr();
+        $data = $this->getData();
+        return array_merge($attr, array('data' => $data));
+    } 
+
+
+    
+    /**
+     * 渲染 js 初始化代码
+     */
+    public function renderInit()
+    {
+        $chartId = $this->getChartId();
+        $chartIdDiv = $chartId . '_div';
+        $type = $this->getType();
+        
+        $insterAttr = array();
+        foreach ($this->insertChartsAttr as $key => $attr) {
+            $insterAttr[] = "{$key}: '{$attr}'";  
+        }
+        
+        $insterAttr = implode(', ', $insterAttr);
+        
+        $params = "type: '{$type}', id: '{$chartId}'";
+        $dataSource = json_encode($this->getDataSource());
+        
+        $html = <<<INSTER
+       
+        <div id="{$chartIdDiv}" class="">xx</div>
+        
+        <script>
+        $("#{$chartIdDiv}").insertFusionCharts({
+               {$params},
+               {$insterAttr},
+               dataSource: {$dataSource}
+    
+         });
+         </script>    
+                    
+INSTER;
+        
+        return $html;
+      
+    }
+
+
+    public function set_caption($val)
+    {
+        $this->caption = $val;
+    }
+
+
+    public function __set($name, $value) {
+        $this->chartAttr[$name] = $value;
+    }
+    
+    
+    
+    public static function auto_load($class) 
+    {   
+        
+        $f = str_replace('Chart', '', $class);
+        include_once dirname(__FILE__) . '/' . $f . '.php';
+    }
     
 }
 
+function pr($data) {
+    
+    echo '<pre>';
+    print_r($data);
+    echo '</pre>';
+}
+
+
+spl_autoload_register('baseChart::auto_load');
 
 
 
--- typemodule/column.php
+++ typemodule/column.php
@@ -5,10 +5,65 @@ class columnChart extends baseChart
     
     protected $_typeId = 'Column';
     
+    
+    private $_trendLinesData = array();
+            
     function __construct() {
         parent::__construct();
     }
     
+    
+    
+    public $extendsDataAction = array('get_trendLines');
+    
+    /*设置chart 属性*/
+    
+    public function set_xAxisName($val)
+    {
+        $this->xAxisName = $val;
+    }
+    
+    public function set_yAxisName($val)
+    {
+        $this->yAxisName = $val;
+    }
+    
+    
+    public function set_numberPrefix($val)
+    {
+        $this->numberPrefix = $val;
+    }
+    
+    
+    public function set_showValues($val)
+    {
+        $this->showValues = $val;
+    }
+    
+    
+    
+    
+    public function f_trendLines($val, $title, $color = '009933')
+    {
+        $this->_trendLinesData[] = array(
+            'startValue' => $val, 
+            'displayvalue' => $title, 
+            'color' => $color
+        );
+        
+    }
+    
+    
+    public function get_trendLines($val, $title, $color = '009933')
+    { 
+        if (empty($this->_trendLinesData))
+            return array();
+        
+        return array('trendlines' => array(
+            'line' => $this->_trendLinesData
+        ));
+    }
+            
     
     
 }
